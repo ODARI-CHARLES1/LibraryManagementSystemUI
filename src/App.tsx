e225d44e2ce6e8562802b3395d90451264375284
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-09 15:46:41 +0300

    implemenation of initial localstorage keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import Sidebar from "./Components/Sidebar"
+import Dashboard from "./Pages/Dashboard";
+if (localStorage.getItem("sideActive")) { ;
+} else {
+  const sideActiveInitial: number = 0;
+  localStorage.setItem("sideActive", JSON.stringify(sideActiveInitial));
+}
+if(localStorage.getItem("Period")){
+  ;
+}
+else{
+  localStorage.setItem("Period","select Period")
+}
 const App = () => {
   return (
-    <div className="bg-amber-50">
-      <Sidebar/>
+    <div className="bg-gray-100">
+      <Dashboard/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
