ed34ff254e7b5392265cc92248dc2e05dd3fecae
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-19 18:39:02 +0300

    update factored the code

--- src/Components/Profile/UserProfile.tsx
+++ src/Components/Profile/UserProfile.tsx
@@ -1,7 +1,12 @@
 import { MdModeEditOutline } from "react-icons/md";
+import { useContext } from "react";
+import appContext from "../../Contexts/AppContext";
+
 const UserProfile = () => {
+  const { theme } = useContext(appContext);
+  
   return (
-    <div className="w-[320px] relative h-fit cursor-pointer p-4 shadow-md rounded-lg bg-white flex items-center gap-4 hover:shadow-lg transition-shadow">
+    <div className={`w-[320px] relative h-fit cursor-pointer p-4 shadow-md rounded-lg flex items-center gap-4 hover:shadow-lg transition-shadow ${theme === "light" ? "bg-white" : "bg-gray-800"}`}>
       
       <div className="shrink-0">
         <img
@@ -11,18 +16,18 @@ const UserProfile = () => {
         />
       </div>
 
-      <div className="w-px self-stretch bg-gray-200"></div>
+      <div className={`w-px self-stretch ${theme === "light" ? "bg-gray-200" : "bg-gray-600"}`}></div>
 
       <div className="flex flex-col justify-center">
         <h1 className="text-lg font-semibold text-green-600 leading-tight">
           Odari K. Charles
         </h1>
 
-        <p className="text-gray-500 text-sm">Admin • Librarian</p>
+        <p className={`text-sm ${theme === "light" ? "text-gray-500" : "text-gray-400"}`}>Admin • Librarian</p>
 
-        <p className="text-gray-700 text-sm mt-1">Odari.charlesk@gmail.com</p>
+        <p className={`text-sm mt-1 ${theme === "light" ? "text-gray-700" : "text-gray-300"}`}>Odari.charlesk@gmail.com</p>
       </div>
-      <MdModeEditOutline className='size-4 text-gray-500 absolute top-4 right-4' />
+      <MdModeEditOutline className={`size-4 absolute top-4 right-4 ${theme === "light" ? "text-gray-500" : "text-gray-400"}`} />
     </div>
   );
 };
