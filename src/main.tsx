1eaaee1bbd1987d34f2d0716ed1e6dda7f6c03e8
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-19 12:22:29 +0300

    configure store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AppProvider } from './Contexts/AppProvider.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppProvider } from "./Contexts/AppProvider.tsx";
+import { store, persistor } from "./App/store.ts";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
-  <AppProvider>
-    <App />
-  </AppProvider>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </PersistGate>
+  </Provider>
+);
