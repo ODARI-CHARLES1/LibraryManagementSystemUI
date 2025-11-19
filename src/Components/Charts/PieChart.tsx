7760335891c9b033ed7ee6cbcaa893ad09a5b32d
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-19 18:37:40 +0300

    update changed color scheme

--- src/Components/Charts/PieChart.tsx
+++ src/Components/Charts/PieChart.tsx
@@ -1,19 +1,28 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useContext } from 'react';
+import appContext from '../../Contexts/AppContext';
 
-import {PieChart} from '@mui/x-charts/PieChart'
 export default function BasicPie() {
+  const { theme } = useContext(appContext);
+
   return (
     <PieChart
       series={[
         {
           data: [
-            { id: 0, value: 10, label: 'Borrowed Books' },
-            { id: 1, value: 15, label: 'Returned Books' },
-            { id: 2, value: 20, label: 'Overdue Books' },
+            { id: 0, value: 45, label: 'Borrowed Books', color: '#22c55e' },
+            { id: 1, value: 30, label: 'Returned Books', color: '#3b82f6' },
+            { id: 2, value: 15, label: 'Overdue Books', color: '#ef4444' },
           ],
         },
       ]}
       width={200}
       height={200}
+      sx={{
+        '.MuiChartsLegend-series text': {
+          fill: theme === 'light' ? '#374151' : '#d1d5db',
+        }
+      }}
     />
   );
 }
