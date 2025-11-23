337ae336ab77f23906f964529143e3060b1d51d8
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-23 13:59:42 +0300

    update concat BookAPI

--- src/App/store.ts
+++ src/App/store.ts
@@ -2,9 +2,11 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 import { userApi } from "../Features/Auth/userApi";
+import { bookAPI } from "../Features/Books/bookAPI";
 
 const rootReducer = combineReducers({
   [userApi.reducerPath]: userApi.reducer,
+  [bookAPI.reducerPath]:bookAPI.reducer,
 });
 
 const persistConfig = {
@@ -20,7 +22,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(userApi.middleware),
+    }).concat(userApi.middleware).concat(bookAPI.middleware),
 });
 
 export const persistor = persistStore(store);
