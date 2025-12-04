812b1dfd8292e700b6b569f9c24c64be9f08443c
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-12-04 23:57:01 +0300

    added user types

--- src/Features/Auth/userApi.ts
+++ src/Features/Auth/userApi.ts
@@ -1,50 +1,50 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { ApiDomain } from "../../Utils/APIDomain";
 
-export interface User{
-    user_id:number,
+export interface User {
+  user_id: number,
+  username: string,
+  email: string,
+  password: string,
+  password_hash?: string,
+  role: 'Admin' | 'Member',
+  created_at?: Date,
+  updated_at?: Date
+}
+
+export interface newUser {
+  username: string,
+  email: string,
+  password: string,
+  role: 'Admin' | 'Member',
+  created_at?: Date,
+  password_hash?: string
+}
+
+export interface updateUser {
+  username?: string,
+  password?: string,
+  role?: 'Admin' | 'Member',
+  updated_at?: Date
+}
+
+
+export interface existingUser {
+  email: string,
+  password: string,
+  password_hash?: string
+}
+
+export interface loginJwtConfig {
+  payload: {
+    id: number
     username: string,
-    email: string,
-    password:string,
-    password_hash?:string,
-    role: 'Admin' | 'Member',
-    created_at?: Date,
-    updated_at?: Date
-}
-
-export interface newUser{
-    username:string,
-    email:string,
-    password:string,
-    role:'Admin' | 'Member',
-    created_at?:Date,
-    password_hash?:string
-}
-
-export interface updateUser{
-    username?:string,
-    password?:string,
-    role?:'Admin' |'Member',
-    updated_at?:Date
-}
-
-
-export interface existingUser{
-    email:string,
-    password:string,
-    password_hash?:string
-}
-
-export interface loginJwtConfig{
-    payload:{
-        id:number
-        username:string,
-        role:string
-        created:Date
-        updated:Date
-    },
-    expires:string,
-    secret:string
+    role: string
+    created: Date
+    updated: Date
+  },
+  expires: string,
+  secret: string
 
 }
 
@@ -64,7 +64,7 @@ export const userApi = createApi({
   }),
   tagTypes: ["Users"],
   endpoints: (builder) => ({
-    createUser: builder.mutation<User, Partial<User>>({
+    createUser: builder.mutation<CreateUserResponse, Partial<User>>({
       query: (newUser) => ({
         url: "api/users/create",
         method: "POST",
@@ -73,30 +73,33 @@ export const userApi = createApi({
       invalidatesTags: ["Users"],
     }),
 
-    loginUser: builder.mutation<User, Partial<User>>({
+    loginUser: builder.mutation<LoginResponse, Partial<User>>({
       query: (loginUser) => ({
         url: "api/users/login",
         method: "POST",
         body: loginUser,
       }),
     }),
-    
-    getUser:builder.mutation<User,Partial<User>>({
-        query:(user)=>({
-            url:"api/users",
-            method:"GET",
-            body:user
-        }),
-        invalidatesTags:["Users"]
-    }),
 
+    getUser: builder.query<User[], void>({
+      query: () => "api/users",
+      providesTags: ["Users"],
+    }),
     UpdateUser: builder.mutation<User, Partial<User>>({
-      query: (updateUserInf) => ({
-        url: "api/users/create",
-        method: "POST",
-        body: updateUserInf,
+      query: ({ user_id, ...rest }) => ({
+        url: `api/users/${user_id}`,
+        method: "PUT",
+        body: rest,
       }),
       invalidatesTags: ["Users"],
     }),
+
+    deleteUser:builder.mutation<{ message: string }, number>({
+      query:(id)=>({
+        url: `api/users/delete/${id}`,
+        method:'DELETE',
+      })
+    })
+
   }),
 });
